ce28a89af0127bc1076edd186452d5c09725fbbb
Vinayak <102165372+kvinayakabhat@users.noreply.github.com>
2024-03-04 15:31:12 +0530

    Add files via upload

---
+++ Array/removeDuplicates.c
@@ -0,0 +1,10 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int i, j;
+    for (i = 0, j = 0; i < numsSize - 1; i++) {
+        if (nums[i] != nums[i + 1]) {
+            nums[j++] = nums[i];
+        }
+    }
+    nums[j++] = nums[numsSize - 1];
+    return j;
+}
