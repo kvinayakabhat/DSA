708457b55c8ccb30e7b3f70c3ab726f3acfdcf46
Vinayak <102165372+kvinayakabhat@users.noreply.github.com>
2026-01-12 17:18:39 +0530

    Twosum problem
    - tried brute force approach with O(n2) 
    - learnt about HashMap
    - unordered_map in C++
    - stored in key value pairs
    - fast lookups by converting key into an index in an internal array
    - key-> hash func -> index -> value
    - O(1)
    - mp.find() for search
    - mp.end() end of the array and ele not found iterator
    - mp[] insert/update

---
+++ Array/TwoSum.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0; i<(int)nums.size();i++){
+            int x = target-nums[i];
+            if(mp.find(x)!=mp.end()){
+                return{mp[x],i};
+            }
+            mp[nums[i]]=i;
+        }
+    return{};
+    }
+
+};
